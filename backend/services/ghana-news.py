dc92b62da726dba2f060a39036a651ff5f3a21ca
Prince Kojo Dugboryele <95886564+pryyyynz@users.noreply.github.com>
2025-05-04 06:30:02 +0000

    local api tests

--- backend/services/ghana-news.py
+++ backend/services/ghana-news.py
@@ -7,19 +7,29 @@ load_dotenv()
 
 api_key = os.environ.get('GHANA_NEWS')
 
-# Base URL for the NewsData.io API
-base_url = "https://newsdata.io/api/1/latest"
+# # Base URL for the NewsData.io API
+# base_url = "https://newsdata.io/api/1/latest"
 
-# Parameters for the API request
-params = {
-    'apikey': api_key,
-    'language': 'en',
-    'country': 'gh',
-    'category': 'business, domestic, education, entertainment, politics',
-}
+# # Parameters for the API request
+# params = {
+#     'apikey': api_key,
+#     'language': 'en',
+#     'country': 'gh',
+#     'category': 'business, domestic, education, entertainment, politics',
+# }
 
-# Make the GET request
-response = requests.get(base_url, params=params)
+# # Make the GET request
+# response = requests.get(base_url, params=params)
 
-print(response.json())
+# print(response.json())
 
+
+import requests
+
+url = "https://api.apitube.io/v1/news/everything"
+
+querystring = {"per_page":"10", "api_key":api_key, "country": "gh", "category": "politics, environment, business, sports"}
+
+response = requests.request("GET", url, params=querystring)
+
+print(response.text)
